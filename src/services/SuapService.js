dfc09bffde299e27b08e53654f3f8f05832a00f5
rafaelscarpelli <rafaelscarpelli715@gmail.com>
2026-09-06 16:23:50 -0300

    deploy

--- src/services/SuapService.js
+++ src/services/SuapService.js
@@ -1,37 +1,12 @@
 const SUAP_AUTH_URL = 'https://suap.ifpr.edu.br/o/authorize/';
-const SUAP_USER_URL = 'https://suap.ifpr.edu.br/api/eu/';
 
 export const gerarUrlLoginSuap = () => {
   const params = new URLSearchParams({
     response_type: 'token',
     client_id: process.env.SUAP_CLIENT_ID,
     redirect_uri: process.env.SUAP_REDIRECT_URI,
+    scope: 'identificacao',
   });
 
   return `${SUAP_AUTH_URL}?${params.toString()}`;
-};
-
-
-export const consultarUsuarioSuap = async (accessToken) => {
-  if (!accessToken) {
-    throw new Error('Access token do SUAP não fornecido.');
-  }
-
-  const response = await fetch(SUAP_USER_URL, {
-    method: 'GET',
-    headers: {
-      Authorization: `Bearer ${accessToken}`,
-      Accept: 'application/json',
-    },
-  });
-
-  if (!response.ok) {
-    const texto = await response.text();
-
-    throw new Error(
-      `Erro ao consultar usuário no SUAP: ${response.status} ${texto}`
-    );
-  }
-
-  return await response.json();
 };
