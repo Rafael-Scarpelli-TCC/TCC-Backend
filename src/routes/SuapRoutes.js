cf38fd254ff73c6486d6fa8a1ae6bd05ec4b5f7a
rafaelscarpelli <rafaelscarpelli715@gmail.com>
2026-09-06 16:26:34 -0300

    deploy

--- src/routes/SuapRoutes.js
+++ src/routes/SuapRoutes.js
@@ -1,8 +1,5 @@
 import express from 'express';
-import {
-  gerarUrlLoginSuap,
-  consultarUsuarioSuap,
-} from '../services/SuapService.js';
+import { gerarUrlLoginSuap } from '../services/SuapService.js';
 
 const router = express.Router();
 
@@ -19,33 +16,5 @@ router.get('/', (req, res) => {
     });
   }
 });
-
-router.post('/usuario', async (req, res) => {
-  try {
-    const { accessToken } = req.body;
-
-    if (!accessToken) {
-      return res.status(400).json({
-        message: 'Access token não fornecido.',
-      });
-    }
-
-    const usuarioSuap = await consultarUsuarioSuap(accessToken);
-
-    res.json({
-      message: 'Usuário do SUAP consultado com sucesso.',
-      usuario: usuarioSuap,
-    });
-
-  } catch (error) {
-    console.error('Erro ao consultar usuário SUAP:', error);
-
-    res.status(500).json({
-      message: 'Erro ao consultar usuário no SUAP.',
-      error: error.message,
-    });
-  }
-});
-
 
 export default router;
